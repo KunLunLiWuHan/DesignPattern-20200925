6489e01c4157284789388e8704ee28549bfb77f7
小伦 <43144822+KunLunLiWuHan@users.noreply.github.com>
2020-09-25 10:35:29 +0800

    类对象同步

--- DesignPattern/src/com/atguigu/singleton/type6/SingletonTest06.java
+++ DesignPattern/src/com/atguigu/singleton/type6/SingletonTest06.java
@@ -26,6 +26,7 @@ class Singleton {
 			 * 锁，此时b等待；当a创建对象完成之后，释放锁，b进入，但是进入后发现if (instance ！= null)
 			 * 就很快退出。
 			 * 在大if()之外的代码此时不会符合条件，直接返回instance。
+			 * 同步的是类对象,在不同线程中,只要是这个类的对象都会同步等待。
 			 */
 			synchronized (Singleton.class) {
 				if (instance == null) {
